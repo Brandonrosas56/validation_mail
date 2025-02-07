c79beac1e718f37498fd8c3e60c36a4b864a0e97
dianavelez011 <d.velezposso@gmail.com>
2025-02-07 09:38:47 -0500

    nemotecnia

--- app/Models/Solicitante.php
+++ app/Models/Solicitante.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Solicitante extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'regional',
+        'primer_nombre',
+        'segundo_nombre',
+        'primer_apellido',
+        'segundo_apellido',
+        'correo_personal',
+        'numero_contrato',
+        'fecha_inicio_contrato',
+        'fecha_terminacion_contrato',
+    ];
 }
+
