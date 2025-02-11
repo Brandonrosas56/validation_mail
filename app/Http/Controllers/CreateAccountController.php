3ddf82f4f260183174f491ac41aef1c0aa29969e
japalacioSena <japalacio@sena.eu.co>
2025-02-11 10:22:47 -0500

    Contractor restriction to access data to validation account

--- app/Http/Controllers/CreateAccountController.php
+++ app/Http/Controllers/CreateAccountController.php
@@ -60,9 +60,9 @@ class CreateAccountController extends Controller
 
 
         // Validar con la API del SECOP
-        // if (!$this->validarContratoSecop($documentoProveedor, $numeroContrato, $estadoContrato)) {
-        //     return redirect()->back()->with('error', 'El contrato no está vigente según el SECOP.');
-        // }
+        if (!$this->validarContratoSecop($documentoProveedor, $numeroContrato, $estadoContrato)) {
+            return redirect()->back()->with('error', 'El contrato no está vigente según el SECOP.');
+        }
 
         // Guardar los datos si pasa la validación
         CreateAccount::create($request->all());
