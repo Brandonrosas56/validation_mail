36d85b661b7af1de7dee80a054e433b2936cf875
Brandonrosas56 <brayanrosas635@gmail.com>
2025-02-12 11:57:21 -0500

    web

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\App;
 use App\Http\Middleware\CheckRole;
 use App\Http\Middleware\CheckIfBlocked;
 use App\Http\Controllers\GlpiController;
+use App\Http\Controllers\TicketController;
 
 
 App::setLocale('es');
@@ -71,9 +72,9 @@ Route::middleware(['auth', 'checkIfBlocked'])->group(function () {
        
                 });
                 Route::middleware('auth')->prefix('glpi')->group(function () {
-                Route::get('/init-session', [GlpiController::class, 'initSession']);
-                Route::get('/ticket/{id}', [GlpiController::class, 'getTicket']);
-                Route::post('/ticket', [GlpiController::class, 'createTicket']);
+                Route::get('/init-session', [TicketController::class, 'initSession']);
+                Route::get('/ticket/{id}', [TicketController::class, 'getTicket']);
+                Route::post('/ticket', [TicketController::class, 'createTicket']);
                 });
                 
             });
