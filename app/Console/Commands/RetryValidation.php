d1694105950e15fa1a9112fcb4fc9ff9e0058c9c
dianavelez011 <d.velezposso@gmail.com>
2025-02-13 12:04:33 -0500

    faker

--- app/Console/Commands/RetryValidation.php
+++ app/Console/Commands/RetryValidation.php
@@ -31,6 +31,23 @@ class RetryValidation extends Command
 
 
         $faker = Faker::create();
+        $userData = [
+            'rgn_id' => $faker->numberBetween(1, 10), // ID regional (puedes ajustar el rango)
+            'primer_nombre' => $faker->firstName,
+            'segundo_nombre' => $faker->firstName,
+            'primer_apellido' => $faker->lastName,
+            'segundo_apellido' => $faker->lastName,
+            'correo_personal' => $faker->safeEmail,
+            'correo_institucional' => $faker->unique()->companyEmail,
+            'numero_contrato' => $faker->regexify('SECOP-\d{6}'), // Contrato con un patrón específico
+            'fecha_inicio_contrato' => $faker->date('Y-m-d', 'now'),
+            'fecha_terminacion_contrato' => $faker->date('Y-m-d', '+1 year'),
+            'usuario' => $faker->userName,
+        ];
+
+     $sendemailValidationStatusService = new SendValidationStatusService($userData, SendValidationStatusService::NEMOTECNIA_ERROR
+    );
+    $sendemailValidationStatusService->sendTicket();
 
         
 }
