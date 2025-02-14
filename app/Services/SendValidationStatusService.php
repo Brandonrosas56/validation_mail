1d36e62f735aba83c678c1c41cfe4719c89e892b
dianavelez011 <d.velezposso@gmail.com>
2025-02-14 14:20:23 -0500

    actualizacion plantillas

--- app/Services/SendValidationStatusService.php
+++ app/Services/SendValidationStatusService.php
@@ -56,19 +56,22 @@ class SendValidationStatusService
             'input' => [
                 'name' => "Caso por Nemotecnia - Contratista (Fallido)",
                 'content' => "
-                         *Datos del Usuario:*
-                         * *Regional:* {$this->userData['rgn_id']}
-                         * *Primer Nombre:* {$this->userData['primer_nombre']}
-                         * *Segundo Nombre:* {$this->userData['segundo_nombre']}
-                         * *Primer Apellido:* {$this->userData['primer_apellido']}
-                         * *Segundo Apellido:* {$this->userData['segundo_apellido']}
-                         * *Usuario:* {$this->userData['usuario']}
-                     ",
-                'type' => 2,
+                        *Datos del Usuario:*
+                        * *Regional:* {$this->userData['rgn_id']}
+                        * *Primer Nombre:* {$this->userData['primer_nombre']}
+                        * *Segundo Nombre:* {$this->userData['segundo_nombre']}
+                        * *Primer Apellido:* {$this->userData['primer_apellido']}
+                        * *Segundo Apellido:* {$this->userData['segundo_apellido']}
+                        * *Usuario:* {$this->userData['usuario']}
+                    ",
+                'type' => 1,
                 'status' => 1,
-                'urgency' => 3,
+                'urgency' => 4,
                 'impact' => 3,
                 'requesttypes_id' => 1,
+                'groups_id' => 5, // ID del grupo asignado
+                'users_id_assign' => 10, // ID del técnico asignado
+
             ]
         ];
     }
@@ -92,11 +95,14 @@ class SendValidationStatusService
                         * *Fecha de Terminación del Contrato:* {$this->userData['fecha_terminacion_contrato']}
                         * *Usuario:* {$this->userData['usuario']}
                     ",
-                'type' => 2,
+                'type' => 1,
                 'status' => 1,
-                'urgency' => 3,
+                'urgency' => 4,
                 'impact' => 3,
                 'requesttypes_id' => 1,
+                'groups_id' => 5, // ID del grupo asignado
+                'users_id_assign' => 10, // ID del técnico asignado
+
             ]
         ];
 
@@ -120,11 +126,14 @@ class SendValidationStatusService
                         * *Fecha de Terminación del Contrato:* {$this->userData['fecha_terminacion_contrato']}
                         * *Usuario:* {$this->userData['usuario']}
                     ",
-                'type' => 2,
+                'type' => 1,
                 'status' => 1,
-                'urgency' => 3,
+                'urgency' => 4,
                 'impact' => 3,
                 'requesttypes_id' => 1,
+                'groups_id' => 5, // ID del grupo asignado
+                'users_id_assign' => 10, // ID del técnico asignado
+
             ]
         ];
     }
@@ -142,13 +151,16 @@ class SendValidationStatusService
                             * *Segundo Apellido:* {$this->userData['segundo_apellido']}
                             * *Usuario:* {$this->userData['usuario']}
                         ",
-                'type' => 3,
-                'status' => 3,
-                'urgency' => 3,
-                'impact' => 3,
-                'requesttypes_id' => 1,
-            ]
-        ];
+                    'type' => 1,
+                    'status' => 1,
+                    'urgency' => 4,
+                    'impact' => 3,
+                    'requesttypes_id' => 1,
+                    'groups_id' => 5, // ID del grupo asignado
+                    'users_id_assign' => 10, // ID del técnico asignado
+
+                ]
+            ];
 
     }
 }
