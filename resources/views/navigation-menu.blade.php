4ff1b28610a20fa5912389ef03f0ba609df9f680
Alexander Palacio <japalacio@sena.edu.co>
2025-02-16 17:04:56 -0500

    hidden profile

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -119,9 +119,9 @@
                             Adminstrar Cuenta
                         </div>
 
-                        <x-dropdown-link href="{{ route('profile.show') }}">
+                        <!-- <x-dropdown-link href="{{ route('profile.show') }}">
                             {{ __('Profile') }}
-                        </x-dropdown-link>
+                        </x-dropdown-link> -->
 
                         @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
                         <x-dropdown-link href="{{ route('api-tokens.index') }}">
@@ -180,9 +180,9 @@
 
             <div class="mt-3 space-y-1">
                 <!-- Account Management -->
-                <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
+                <!-- <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
                     {{ __('Profile') }}
-                </x-responsive-nav-link>
+                </x-responsive-nav-link> -->
 
                 @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
                 <x-responsive-nav-link href="{{ route('api-tokens.index') }}" :active="request()->routeIs('api-tokens.index')">
